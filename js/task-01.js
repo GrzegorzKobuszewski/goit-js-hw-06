db036ca134b73438e89b2b1fc126ddc0c286dfe0
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-16 14:55:45 +0200

    Zadanie 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,8 @@
-const numberOfCategories = document.querySelector("#ul").length;
-console.log(`Number of categories: ${numberOfCategories}`);
+const arrayOfCategories = document.querySelectorAll("#categories>li.item");
+console.log(`Number of categories: ${arrayOfCategories.length}`);
+
+const categoriesOfItem = document.querySelectorAll("#categories>li")
+    .forEach (e => {
+        console.log(`Category: ${e.firstElementChild.textContent}`);
+        console.log(`Elements: ${e.lastElementChild.children.length}`);
+    });
