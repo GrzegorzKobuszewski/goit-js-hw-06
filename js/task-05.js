09ca8b57dbb2f3a1ded034179743d4f3ee191d20
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-17 15:19:31 +0200

    Zadanie 6
    ZADANIE: Po kliknieciu w inne miejsce (zdjęcie efektu focus – uzyskano efekt blur, czyli rozproszenie) pole formularza świeci się na czerwono, gdy nie ma odpowiedniej liczby znaków… jeżeli jest odpowiednia liczba znaków, to skrypt wykonuje ustawienie klasy atrybutów na valid, czyli na zielono
    
    słowa kluczowe: blur, zdarzenia, focus, setAttribute, ustawianie klasy/atrybutu
    
    Napisz skrypt, który przy utracie fokusu na polu input (zdarzenie blur) sprawdza jego zawartość na prawidłową liczbę wprowadzonych symboli.
    
    <input
      type="text"
      id="validation-input"
      data-length="6"
      placeholder="Please enter 6 symbols"
    />
    
        Informacja o liczbie symboli, która powinna znajdować się w polu input, pokazuje się w jego atrybucie data-length.
        Jeśli wprowadzono odpowiednią liczbę symboli, to border pola input staje się zielone, a jeśli liczba jest nieprawidłowa - czerwone.
    
    Aby dodać style, używaj klas CSS valid i invalid, które już dodaliśmy do plików źródłowych zadania.
    
    #validation-input {
      border: 3px solid #bdbdbd;
    }
    
    #validation-input.valid {
      border-color: #4caf50;
    }
    
    #validation-input.invalid {
      border-color: #f44336;
    }

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,7 @@
 const input = document.querySelector('input#name-input');
 const output = document.querySelector('span#name-output');
 
-let inputValue = "Anonymous";
-
 input.addEventListener("input", (e) => {
-    if (e.currentTarget.value===0) output.textContent = inputValue
+    if (!e.currentTarget.value) output.textContent = "Anonymous"
     else output.textContent = e.currentTarget.value;
 });
-
-
---
+++ js/task-06.js
@@ -0,0 +1,6 @@
+const textInput = document.querySelector("input#validation-input");
+
+textInput.addEventListener("blur", (e) => {
+    if (e.currentTarget.value.length === 6) e.currentTarget.setAttribute("class", "valid")
+    else e.currentTarget.setAttribute("class", "invalid");
+});
