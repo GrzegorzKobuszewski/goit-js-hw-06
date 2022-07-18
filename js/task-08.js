3ca8caa8dab4bb14e45413016b76908154048996
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-18 10:42:49 +0200

    Zadanie 8 poprawka
    było kilka błędów logicznych (użycie złych metod) i źle dobrałem parametry

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,14 @@
+const form = document.querySelector("form.login-form");
 
-const form = document.addEventListener("form.login-form");
-const submit = document.querySelector(`form>button[type="submit"]`);
-
-submit.addEventListener("submit", e => {
+form.addEventListener("submit", e => {
     e.preventDefault();
-    const { elements: { login, password } } = e.currentTarget;
+    const { elements: { email, password } } = e.currentTarget;
     
-    if (login.value === "" || password.value === "") alert("Wypełnij wszystkie pola formularza");
-    if (login.value !== "" && password.value !== "") {
-        console.log (`Login: ${login.value}, Password: ${password.value}`);
-        e.currentTarget.reset();
-    }
+    if (email.value === "" || password.value === "") {
+        return alert("Wypełnij wszystkie pola formularza");
+    };
+
+    const userData = { email: email.value, password: password.value }
+    console.log (userData);
+    e.currentTarget.reset();
 });
