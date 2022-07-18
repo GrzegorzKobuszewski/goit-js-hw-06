5efff9240ae47f5ac7e99bd5a41b172893974217
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-07-18 10:23:49 +0200

    Zadanie 8
    Skrypt formularza, który sprawdza czy użytkownik wypełnił wszystkie pola formularza i jeśli nie to zwraca komentarz. Formularz nie resetuje się po wprowadzeniu danych, pola są czyszczone po wysłaniu formularza.
    
    Słowa kluczowe:
    resetowanie formularza, czyszczenie wartości pól metodą reset,
    
    Metody i funkcje:
    e.preventDefault();
    e.currentTarget.reset();
    .addEventListener
    .addEventListener
    
    Napisz skrypt zarządzania formularzem logowania.
    
    <form class="login-form">
      <label>
        Email
        <input type="email" name="email" />
      </label>
      <label>
        Password
        <input type="password" name="password" />
      </label>
      <button type="submit">Login</button>
    </form>
    
        Opracowanie przesyłania formularza form.login-form powinno przebiegać zgodnie ze zdarzeniem submit.
        Podczas przesyłania formularza strona nie powinna się restartować.
        Jeśli w formularzu są nieuzupełnione pola, wprowadź alert z upomnieniem o tym, że wszystkie pola powinny zostać wypełnione.
        Jeśli użytkownik uzupełnił wszystkie pola i wysłał formularz, zbierz wartość pól jako obiekt, gdzie nazwa pola będzie nazwą właściwości, a wartość pola - wartością właściwości. Aby otrzymać dostęp do elementów formularza użyj właściwości elements.
        Umieść obiekt z wprowadzonymi danymi do wiersza poleceń i wyczyść wartości pól formularza metodą reset.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
 
+const form = document.addEventListener("form.login-form");
+const submit = document.querySelector(`form>button[type="submit"]`);
+
+submit.addEventListener("submit", e => {
+    e.preventDefault();
+    const { elements: { login, password } } = e.currentTarget;
+    
+    if (login.value === "" || password.value === "") alert("Wypełnij wszystkie pola formularza");
+    if (login.value !== "" && password.value !== "") {
+        console.log (`Login: ${login.value}, Password: ${password.value}`);
+        e.currentTarget.reset();
+    }
+});
